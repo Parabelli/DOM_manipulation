cebc16ce172461e4016f17fe4abd108e899343be
Noah <noahnene@outlook.com>
2023-06-06 10:28:02 +0200

    intro done

---
+++ 1_introduction/script.js
@@ -0,0 +1,18 @@
+let title = document.title;
+console.log(title);
+
+title = "Modifying the DOM";
+console.log(title);
+
+let bgColor = (document.body.style.backgroundColor = "#FF69B4");
+
+let r = Math.floor(Math.random() * 255);
+let g = Math.floor(Math.random() * 255);
+let b = Math.floor(Math.random() * 255);
+
+bgColor = document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+console.log(bgColor);
+
+for (const childNodes of document.body.childNodes) {
+  console.log(childNodes);
+}
