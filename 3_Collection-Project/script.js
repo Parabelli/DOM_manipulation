98332d5ce1b9145beb542c75a58afb4a995abc93
Noah <noahnene@outlook.com>
2023-06-12 15:51:01 +0200

    finished desktop

---
+++ 3_Collection-Project/script.js
@@ -0,0 +1,98 @@
+const collection = [
+  {
+    name: `The Myth of Sisyphus`,
+    author: `Albert Camus`,
+    release: `1942`,
+    description: `The Myth of Sisyphus by Albert Camus is a philosophical essay that explores the theme of the absurdity of human existence. The essay takes its name from the Greek mythological figure Sisyphus, who was condemned by the gods to roll a boulder uphill only to watch it roll back down, repeating this futile task for eternity.`,
+    picture: `assets/image/sisyphe.jpg`,
+  },
+  {
+    name: `The Stranger`,
+    author: `Albert Camus`,
+    release: `1942`,
+    description: `The novel begins with Meursault receiving news of his mother's death. Despite societal expectations of grieving and displaying emotions, Meursault remains detached throughout the funeral, causing him to be seen as an outsider and indifferent by those around him. This lack of emotional response to his mother's death sets the tone for the rest of the story.`,
+    picture: "assets/image/etranger.jpg",
+  },
+  {
+    name: `Nineteen Eighty-Four`,
+    author: `Georges Orwell`,
+    release: `1949`,
+    description: `This is a dystopian novel set in a totalitarian society ruled by the Party and its enigmatic leader, Big Brother. The story follows the life of Winston Smith, a low-ranking member of the Party who secretly harbors rebellious thoughts and desires for freedom.`,
+    picture: "assets/image/1984.jpg",
+  },
+  {
+    name: `Brave New World`,
+    author: `Aldous Huxley`,
+    release: `1932`,
+    description: `In this new world, citizens are genetically engineered and conditioned from birth to fit specific roles in society. The society's motto is "Community, Identity, Stability," and its primary goal is to maintain social harmony and prevent individuality and dissent. People are conditioned to embrace consumerism, promiscuity, and the use of a mind-altering drug called Soma to escape any negative emotions or thoughts.
+  
+      The protagonist, Bernard Marx, is an Alpha Plus citizen who feels alienated and dissatisfied with the shallow and superficial nature of his society. He becomes interested in a woman named Lenina Crowne, who represents the idealized citizens of the Brave New World. Together, they visit a reservation where "savages" still live according to traditional values.`,
+    picture: "assets/image/bravenewworld.jpg",
+  },
+  {
+    name: `The Cicero Trilogy`,
+    author: `Robert Harris`,
+    release: `2016`,
+    description: `The Cicero Trilogy is a historical fiction series that brings to life the compelling story of the renowned Roman orator and politician, Marcus Tullius Cicero. The trilogy consists of three novels: "Imperium," "Lustrum", and "Dictator."`,
+    picture: "assets/image/cicero.jpg",
+  },
+  {
+    name: `Thus Spoke Zarathustra`,
+    author: `Friedrich Nietzsche`,
+    release: `1883`,
+    description: `The novel begins with Zarathustra's proclamation of the "death of God" and the necessity for humanity to overcome its reliance on traditional religious and moral values. Zarathustra urges individuals to embrace their own will to power, to seek self-realization, and to strive for personal growth and greatness.
+  
+      Throughout the narrative, Zarathustra encounters various characters representing different aspects of human existence. He engages in conversations and confrontations, challenging their beliefs and offering alternative perspectives on life, morality, and the pursuit of meaning. Zarathustra advocates for the "Übermensch" or the "Superman," an individual who transcends societal norms and creates their own values.`,
+    picture: "assets/image/zarathustra.jpg",
+  },
+  {
+    name: `And Then There Were None`,
+    author: `Agatha Christie`,
+    release: `1939`,
+    description: `The story follows ten individuals who are mysteriously invited to an isolated mansion on a remote island called Soldier Island. The guests arrive, only to discover that their enigmatic host is absent, and they are left in the care of two servants.`,
+    picture: "assets/image/therewerenone.jpg",
+  },
+  {
+    name: `Fahrenheit 451`,
+    author: `Ray Bradbury`,
+    release: `1953`,
+    description: `Set in a future society where books are outlawed, the story revolves around Guy Montag, a fireman whose job is not to extinguish fires but to burn books.
+  
+      In this oppressive society, intellectualism and free thought are suppressed. The government manipulates the masses through constant distractions, mindless entertainment, and the control of information. People are encouraged to live superficially, devoid of critical thinking and introspection.`,
+    picture: "assets/image/451.jpg",
+  },
+  {
+    name: `The Dream of a Ridiculous Man`,
+    author: `Fyodor Dostoevsky`,
+    release: `1877`,
+    description: `The narrative follows an unnamed protagonist who is overwhelmed by existential despair and contemplating suicide. However, one night, he has a profound dream that transforms his outlook on life.`,
+    picture: "assets/image/ridiculous.jpg",
+  },
+  {
+    name: `Le Petit Prince`,
+    author: `Antoine de Saint-Exupéry`,
+    release: `1943`,
+    description: `The story begins when a pilot crashes his plane in the Sahara Desert and encounters the Little Prince, a mysterious boy from another planet. As the pilot repairs his plane, the Little Prince shares his extraordinary adventures and observations about the universe.`,
+    picture: "assets/image/petitprince.jpg",
+  },
+];
+
+const books = document.querySelectorAll(`.book`);
+for (let i = 0; i < books.length; i++) {
+  let picture = document.createElement(`img`);
+  picture.src = collection[i].picture;
+  books[i].children[0].appendChild(picture);
+  let release = document.createTextNode(collection[i].release);
+  books[i].children[1].appendChild(release);
+  let name = document.createTextNode(collection[i].name);
+  books[i].children[2].appendChild(name);
+  let author = document.createTextNode(collection[i].author);
+  books[i].children[3].appendChild(author);
+  let description = document.createTextNode(collection[i].description);
+  books[i].children[4].appendChild(description);
+}
+
+const bookscss = document.querySelectorAll(`release`);
+
+console.log(collection[0]);
+console.log(books);
